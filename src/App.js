d540a23f8c57f6f23df18ee05fd0c1c2fb717b2c
steve <steve@Steves-MBP.fios-router.home>
2019-03-20 12:59:52 -0400

    Add instructions

--- src/App.js
+++ src/App.js
@@ -2,61 +2,99 @@ import React, { Component } from 'react';
 import PlayerCard from './PlayerCard.js';
 import './App.css';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.symbols = ['rock', 'paper', 'scissors'];
     this.state = {
-      colorRed: 'red',
-      colorBlue: 'blue',
-      playerRed: this.symbols[2],
-      playerBlue: this.symbols[1],
-      winner: null,
+
+
+      // set colorRed state to 'red'
+
+
+      // set colorBlue state to 'blue'
+
+
+      // set playerRed state to the last item in symbols array
+
+
+      // set playerBlue state to the second item in symbols array
+
+
+      // initialize a winner state of null value
     };
   }
 
+
   decideWinner = () => {
+    // Below is an example of destructuring state.
+    // Here you can see that playerBlue and playerRed were
+    // extracted from this.state.  Now, instead of using
+    // this.state.playerBlue and this.state.playerRed, you can
+    // simply write playerBlue and playerRed
     const { playerBlue, playerRed } = this.state;
-    if (playerRed === playerBlue) {
-      return "It's a draw !";
-    }
-    if (
-      (playerRed === 'rock' && playerBlue === 'scissors') ||
-      (playerRed === 'paper' && playerBlue === 'rock') ||
-      (playerRed === 'scissors' && playerBlue === 'paper')
-    ) {
-      return 'Red player wins !';
-    }
-    return 'Blue player wins !';
+
+
+    // Write a conditional that returns the string "It's a draw !"
+    // if the values of playerRed state and playerBlue states are equal
+
+
+    // Write a conditional with three conditions.  If playerRed equals 'rock
+    // and playerBlue equals 'scissors', or playerRed equals 'paper' and playerBlue
+    // equals 'rock, or playerRed equals 'scissors' and playerBlue equals 'paper', then
+    // return the string 'Red player wins !'
+    // Any other possability should return the string 'Blue player wins !'
   };
 
+
   runGame = () => {
-    // const index = Math.floor(Math.random() * 3);
-    let counter = 0;
+  
+
+
+    // initialize counter that WILL be redefined to 0
+
+
     const myInterval = setInterval(() => {
       counter++;
+      // add one to counter
       this.setState({
         playerRed: this.symbols[Math.floor(Math.random() * 3)],
         playerBlue: this.symbols[Math.floor(Math.random() * 3)],
         winner: '',
       });
-      if (counter > 40) {
+      // set playerRed state to a random item in symbols array (Remember combined math functions used in previous lessons)
+      // set playerBlue state to a random item in symbols array
+      // set winner state to an empty string
+
+
+      if (if the counter number is greater than 40) {
         clearInterval(myInterval);
         this.setState({ winner: this.decideWinner() });
+        // set the state of winner to decideWinner function call
+        // 
       }
     }, 100);
   };
+
 
   render() {
     return (
       <div className="App">
         <PlayerCard color={this.state.colorRed} symbol={this.state.playerRed} />
+        {/* Add PlayerCard as custom html tag. Pass color prop defined by colorRed state.
+        Pass symbol prop defined as playerRed state */}
         <PlayerCard color={this.state.colorBlue} symbol={this.state.playerBlue} />
+        {/* Add another PlayerCard (Gotta love reusable components!).  Pass color prop defined
+        by colorBlue state.  Pass symbol prop defined as playerRed state. */}
         <p>{this.state.winner}</p>
+        {/* Add paragraph tag that displays the value of winner state */}
         <button onClick={this.runGame}>Run Game</button>
+        {/* Add button with onClick set to call runGame (no bubble needed) */}
       </div>
     );
   }
 }
+
 
 export default App;
--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// const PlayerCard = ({ color, symbol, props }) => {
 const PlayerCard = props => {
+  // The style object below is passed to style attribute in div
+  // so that the style of content changes according to prop values.
+  // This is a great example of reusability!!
   const style = {
-    backgroundColor: props.color,
+    // Define key backgroundValue set to the value color prop
     backgroundImage: 'url(./img/' + props.symbol + '.png)',
   };
 
   return (
     <div style={style} className="player-card">
-      {props.symbol}
+      {/* display symbol prop here */}
     </div>
   );
 };
